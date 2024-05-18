c7c576997670eb1a8aa1a92c5d274d406b42c716
MaksPel <168368720+MaksPel@users.noreply.github.com>
2024-05-18 23:12:59 +0300

    lab5

---
+++ WpfApp5/WpfApp5/Integral.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntegralCalculator
+{
+    public class Integral
+    {
+        // Делегат Func для передачи функции
+        public delegate double Function(double x);
+
+        // Метод для вычисления интеграла методом прямоугольников
+        public static double CalculateRectangleMethod(Function func, double a, double b, int n)
+        {
+            double h = (b - a) / n;
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                double x = a + i * h;
+                sum += func(x);
+            }
+            return sum * h;
+        }
+    }
+}
---
+++ WpfApp5/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using IntegralCalculator; 
+
+namespace WpfIntegralCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Пример использования
+            Integral.Function func = Math.Sin; // Интегрируемая функция - синус
+            double a = double.Parse(AValueTextBox.Text); // Нижний предел интегрирования
+            double b = double.Parse(BValueTextBox.Text); // Верхний предел интегрирования
+            int n = int.Parse(NValueTextBox.Text); // Количество разбиений
+            double result = Integral.CalculateRectangleMethod(func, a, b, n); // Вычисление интеграла
+            ResultLabel.Content = $"Integral value: {result}";
+        }
+    }
+}
