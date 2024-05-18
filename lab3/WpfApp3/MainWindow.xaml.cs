0f4d7a885d4468f623cc42ce7dad4a73bd4e99e1
MaksPel <168368720+MaksPel@users.noreply.github.com>
2024-05-18 23:10:54 +0300

    lab3

---
+++ lab3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+
+namespace BinaryTreeCollection
+{
+    public partial class MainWindow : Window
+    {
+        private BinaryTree<int> binaryTree;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            binaryTree = new BinaryTree<int>();
+
+            // Добавляем несколько элементов для демонстрации
+            binaryTree.Add(50);
+            binaryTree.Add(30);
+            binaryTree.Add(70);
+            binaryTree.Add(20);
+            binaryTree.Add(40);
+            binaryTree.Add(60);
+            binaryTree.Add(80);
+
+            // Отображаем элементы в ListBox
+            foreach (var item in binaryTree)
+            {
+                listBoxItems.Items.Add(item);
+            }
+        }
+
+        private void AddRandomNumber_Click(object sender, RoutedEventArgs e)
+        {
+            // Генерируем случайное число и добавляем его в дерево
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100); // Генерируем число от 1 до 100
+            binaryTree.Add(randomNumber);
+
+            // Очищаем ListBox и обновляем его с новыми элементами
+            UpdateListBox();
+        }
+
+        private void RemoveSelectedItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, выбран ли какой-либо элемент в ListBox
+            if (listBoxItems.SelectedItem != null)
+            {
+                // Удаляем выбранный элемент из коллекции
+                binaryTree.Remove((int)listBoxItems.SelectedItem);
+
+                // Очищаем ListBox и обновляем его с новыми элементами
+                UpdateListBox();
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to remove.", "No Item Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void UpdateListBox()
+        {
+            listBoxItems.Items.Clear();
+            foreach (var item in binaryTree)
+            {
+                listBoxItems.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ lab3/WpfApp3/Tree.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTreeCollection
+{
+    // Класс для узла дерева
+    public class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    // Класс для бинарного дерева
+    public class BinaryTree<T> : ICollection<T>
+    {
+        private BinaryTreeNode<T> root;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => false;
+
+        public BinaryTree()
+        {
+            root = null;
+            Count = 0;
+        }
+
+        // Добавление элемента в дерево
+        public void Add(T item)
+        {
+            if (root == null)
+            {
+                root = new BinaryTreeNode<T>(item);
+            }
+            else
+            {
+                AddRecursive(root, item);
+            }
+            Count++;
+        }
+
+        private void AddRecursive(BinaryTreeNode<T> node, T item)
+        {
+            IComparable<T> comparable = item as IComparable<T>;
+            if (comparable.CompareTo(node.Data) < 0)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new BinaryTreeNode<T>(item);
+                }
+                else
+                {
+                    AddRecursive(node.Left, item);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new BinaryTreeNode<T>(item);
+                }
+                else
+                {
+                    AddRecursive(node.Right, item);
+                }
+            }
+        }
+
+        // Итератор для обхода элементов дерева
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrderTraversal(root).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<T> InOrderTraversal(BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                foreach (var leftNodeData in InOrderTraversal(node.Left))
+                {
+                    yield return leftNodeData;
+                }
+                yield return node.Data;
+                foreach (var rightNodeData in InOrderTraversal(node.Right))
+                {
+                    yield return rightNodeData;
+                }
+            }
+        }
+
+        // Остальные методы ICollection можно реализовать по аналогии
+
+        public void Clear()
+        {
+            root = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return ContainsRecursive(root, item);
+        }
+
+        private bool ContainsRecursive(BinaryTreeNode<T> node, T item)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            IComparable<T> comparable = item as IComparable<T>;
+
+            if (comparable.CompareTo(node.Data) < 0)
+            {
+                return ContainsRecursive(node.Left, item);
+            }
+            else if (comparable.CompareTo(node.Data) > 0)
+            {
+                return ContainsRecursive(node.Right, item);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            if (!Contains(item))
+            {
+                return false;
+            }
+
+            root = RemoveRecursive(root, item);
+            Count--;
+
+            return true;
+        }
+
+        private BinaryTreeNode<T> RemoveRecursive(BinaryTreeNode<T> node, T item)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            IComparable<T> comparable = item as IComparable<T>;
+
+            if (comparable.CompareTo(node.Data) < 0)
+            {
+                node.Left = RemoveRecursive(node.Left, item);
+            }
+            else if (comparable.CompareTo(node.Data) > 0)
+            {
+                node.Right = RemoveRecursive(node.Right, item);
+            }
+            else
+            {
+                if (node.Left == null && node.Right == null)
+                {
+                    return null;
+                }
+                else if (node.Left == null)
+                {
+                    return node.Right;
+                }
+                else if (node.Right == null)
+                {
+                    return node.Left;
+                }
+                else
+                {
+                    BinaryTreeNode<T> minRightNode = FindMinNode(node.Right);
+                    node.Data = minRightNode.Data;
+                    node.Right = RemoveRecursive(node.Right, minRightNode.Data);
+                }
+            }
+
+            return node;
+        }
+
+        private BinaryTreeNode<T> FindMinNode(BinaryTreeNode<T> node)
+        {
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+    }
+}
