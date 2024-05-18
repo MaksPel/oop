f3ea2ae0345b8609d68dfe6b70ed048c94158ac5
MaksPel <168368720+MaksPel@users.noreply.github.com>
2024-05-18 23:15:54 +0300

    lab9

---
+++ lab9/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nСумма положительных чисел: " + File.ReadAllLines(@"../../../numbers.txt").SelectMany(line => line.Split(" ")).Select(word => double.Parse(word)).Where(x => x > 0).Sum());
+    }
+}
